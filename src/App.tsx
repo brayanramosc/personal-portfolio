8dd8ea457d3df75158095dd978815561b78ab3d9
Brayan Ramos <brayanrcaicedo@gmail.com>
2023-12-15 17:52:55 -0500

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useTranslation } from "react-i18next"
-
-import { Header } from "./components";
+import { Header, Hero } from "./components";
 
 function App() {
-  const { t } = useTranslation();
-
   return (
-    <div className="min-h-screen text-primary-light dark:bg-[#121212] dark:text-primary-dark">
+    <div
+      className="min-h-screen text-primary-light dark:bg-[#121212] dark:text-primary-dark"
+    >
       <Header />
-      <h1 className="text-3xl font-bold text-center dark:text-white">
-        {'Hallo Welt! '}{t('title')}
-      </h1>
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from "react-i18next";
+
+import programmingLogo from "../assets/programming.svg";
+
+const Hero = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="px-10">
+            <section className="pt-28 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-20 sm:gap-5">
+                <aside className="flex flex-col justify-center">
+                    <h1 className="text-5xl sm:text-6xl">
+                        {t('heading')}
+                    </h1>
+                    <h1 className="text-5xl sm:text-6xl">
+                        {t('headingName')}
+                    </h1>
+                    <h3 className="text-3xl sm:text-4xl pt-10">
+                        {t('headingSubtitle')}
+                    </h3>
+                </aside>
+
+                <img src={programmingLogo} alt='programming image' />
+            </section>
+        </div>
+    )
+}
+
+export default Hero;
